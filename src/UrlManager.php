2b3696fef2a0df580f7bd4372c4d9931dba0a7a6
Javier Fernández <leqjl93@hotmail.com>
2023-04-30 16:19:30 -0600

    Update UrlManager.php

--- src/UrlManager.php
+++ src/UrlManager.php
@@ -3,12 +3,11 @@
 namespace CapitalPayments\Sdk;
 
 class UrlManager {
-    // const URL_CREATE_INVOICE = 'http://localhost:8888/capitalpayments/app/api/createInvoice';
-    const LOGIN = 'http://localhost:8888/capitalpayments/app/api/login';
-    const CREATE_INVOICE = 'http://localhost:8888/capitalpayments/app/api/createInvoice';
-    const GET_BALANCE = 'http://localhost:8888/capitalpayments/app/api/getBalance';
-    const GET_INVOICE_STATUS = 'http://localhost:8888/capitalpayments/app/api/getInvoiceStatus';
-    const GET_MAIN_WALLET = 'http://localhost:8888/capitalpayments/app/api/getMainWallet';
-    const GET_WALLETS = 'http://localhost:8888/capitalpayments/app/api/getWallets';
-    const GET_ENVIRONMENT = 'http://localhost:8888/capitalpayments/app/api/getEnvironment';
+    const LOGIN = 'https://www.capitalpayments.co/app/api/login';
+    const CREATE_INVOICE = 'https://www.capitalpayments.co/app/api/createInvoice';
+    const GET_BALANCE = 'https://www.capitalpayments.co/app/api/getBalance';
+    const GET_INVOICE_STATUS = 'https://www.capitalpayments.co/app/api/getInvoiceStatus';
+    const GET_MAIN_WALLET = 'https://www.capitalpayments.co/app/api/getMainWallet';
+    const GET_WALLETS = 'https://www.capitalpayments.co/app/api/getWallets';
+    const GET_ENVIRONMENT = 'https://www.capitalpayments.co/app/api/getEnvironment';
 }
