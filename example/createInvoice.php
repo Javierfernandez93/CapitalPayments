d8c271acc92e73e823fc900f0632cee3d1e4d8cd
Javier Fernández <leqjl93@hotmail.com>
2023-04-30 17:48:50 -0600

    Fixes

--- example/createInvoice.php
+++ example/createInvoice.php
@@ -2,7 +2,7 @@
 
 include __DIR__ . "/../vendor/autoload.php";
 
-$Sdk = new CapitalPayments\Sdk\Sdk('mnz6TvEp2MidgjPO','/cJO6a3P5PDy289fEWMo4WP8a3VLI6N8SULuazyteJy55cjxF8Bertn2dSAS+agtPxcrNFLPX1n9nkDt17mkdw==:VEFMRU5UT1VNQlJFTExBMg==');
+$Sdk = new CapitalPayments\Sdk\Sdk('api_key','api_secret');
 
 # creates a new payment and retrieves the data and url payment 
 $response = $Sdk->createInvoice([
