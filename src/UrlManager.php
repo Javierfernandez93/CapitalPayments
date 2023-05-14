2b977378d01b5a04dfa2a4d9ab4d863c77b0b8c2
Javier Fernández <leqjl93@hotmail.com>
2023-05-13 22:45:34 -0600

    Update UrlManager.php

--- src/UrlManager.php
+++ src/UrlManager.php
@@ -4,22 +4,22 @@ namespace CapitalPayments\Sdk;
 
 class UrlManager {
     // account
-    const GET_ENVIRONMENT = 'https://www.capitalpayments.co/app/api/getEnvironment';
-    const GET_ACCOUNT = 'https://www.capitalpayments.co/app/api/getAccount';
-    const LOGIN = 'https://www.capitalpayments.co/app/api/login';
+    const GET_ENVIRONMENT = 'https://www.capitalpayments.me/app/api/getEnvironment';
+    const GET_ACCOUNT = 'https://www.capitalpayments.me/app/api/getAccount';
+    const LOGIN = 'https://www.capitalpayments.me/app/api/login';
 
     // invoices
-    const CREATE_INVOICE = 'https://www.capitalpayments.co/app/api/createInvoice';
-    const GET_INVOICE_STATUS = 'https://www.capitalpayments.co/app/api/getInvoiceStatus';
-    const CANCEL_INVOICE = 'https://www.capitalpayments.co/app/api/cancelInvoice';
+    const CREATE_INVOICE = 'https://www.capitalpayments.me/app/api/createInvoice';
+    const GET_INVOICE_STATUS = 'https://www.capitalpayments.me/app/api/getInvoiceStatus';
+    const CANCEL_INVOICE = 'https://www.capitalpayments.me/app/api/cancelInvoice';
     
     // payouts
-    const CREATE_PAYOUT = 'https://www.capitalpayments.co/app/api/createPayout';
-    const GET_PAYOUT_STATUS = 'https://www.capitalpayments.co/app/api/getPayoutStatus';
-    const CANCEL_PAYOUT = 'https://www.capitalpayments.co/app/api/cancelPayout';
+    const CREATE_PAYOUT = 'https://www.capitalpayments.me/app/api/createPayout';
+    const GET_PAYOUT_STATUS = 'https://www.capitalpayments.me/app/api/getPayoutStatus';
+    const CANCEL_PAYOUT = 'https://www.capitalpayments.me/app/api/cancelPayout';
 
     // wallet
-    const GET_BALANCE = 'https://www.capitalpayments.co/app/api/getBalance';
-    const GET_MAIN_WALLET = 'https://www.capitalpayments.co/app/api/getMainWallet';
-    const GET_WALLETS = 'https://www.capitalpayments.co/app/api/getWallets';
+    const GET_BALANCE = 'https://www.capitalpayments.me/app/api/getBalance';
+    const GET_MAIN_WALLET = 'https://www.capitalpayments.me/app/api/getMainWallet';
+    const GET_WALLETS = 'https://www.capitalpayments.me/app/api/getWallets';
 }
